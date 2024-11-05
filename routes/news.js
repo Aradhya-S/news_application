3126286a7b0d0f6fa51a8413cce051441cef68b5
Ananya Sharma <ananyasharma3064@gmail.com>
2024-11-05 14:55:16 +0530

    final

---
+++ routes/news.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const axios = require('axios')
+const newsr=express.Router()
+const moment = require('moment')
+const math = require('math')
+
+
+newsr.get('/',async(req,res)=>{
+    try {
+        var url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=aaa0dc6482d849d6a6045bbe9154f712';
+
+        const news_get =await axios.get(url)
+        res.render('news',{articles:news_get.data.articles})
+        
+        
+        
+    
+    } catch (error) {
+        if(error.response){
+            console.log(error)
+        }
+        
+    }
+})
+
+newsr.post('/search',async(req,res)=>{
+    const search=req.body.search
+    // console.log(req.body.search)
+
+    try {
+        var url = `http://newsapi.org/v2/everything?q=${search}&apiKey=aaa0dc6482d849d6a6045bbe9154f712`
+
+        const news_get =await axios.get(url)
+        res.render('news',{articles:news_get.data.articles})
+        
+        
+
+        
+        
+    } catch (error) {
+        if(error.response){
+            console.log(error)
+        }
+        
+    }
+})
+
+
+module.exports=newsr
